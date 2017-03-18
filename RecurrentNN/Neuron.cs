39fa4843719d42268db93c08c54660dafe7a7d0c
DangouHaim <gholub1996@mail.ru>
2017-03-18 13:20:25 +0300

    commit1

---
+++ RecurrentNN/Neuron.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrentNN
+{
+    class Neuron
+    {
+        public double[] _in;
+        public double[] _out;
+        public double[,] _wio;
+        public double[] _lim;
+        public double _speed;
+
+        public Neuron(int inputs, int outputs, double[] limits, double speed)
+        {
+            _in = new double[inputs];
+            _out = new double[outputs];
+            _wio = new double[inputs, outputs];
+            _lim = limits;
+            _speed = speed;
+            SetW();
+        }
+
+        void SetW()
+        {
+            Random r = new Random();
+            for(int i = 0; i < _in.Length; i++)
+            {
+                for(int j = 0; j < _out.Length; j++)
+                {
+                    _wio[i, j] = r.NextDouble() * 0.3 + 0.1;
+                }
+            }
+        }
+
+        public void Out()
+        {
+            for(int i = 0; i < _out.Length; i++)
+            {
+                _out[i] = 0;
+                for(int j = 0; j < _in.Length; j++)
+                {
+                    _out[i] += _in[j] * _wio[j, i];
+                }
+                double o = 0;
+                for(int j = 0; j < _lim.Length; j++)
+                {
+                    if(_out[i] > _lim[j] / 2)
+                    {
+                        o = _lim[j];
+                    }
+                }
+                _out[i] = o;
+            }
+        }
+
+        public void SetIn(double[] inputs)
+        {
+            if(_in.Length == inputs.Length)
+            {
+                _in = inputs;
+            }
+            else
+            {
+                for(int i = 0; i < _in.Length; i++)
+                {
+                    _in[i] = inputs[i];
+                }
+            }
+        }
+
+        public void Study(double[] errors)
+        {
+            for(int i = 0; i < _in.Length; i++)
+            {
+                for(int j = 0; j < _out.Length; j++)
+                {
+                    _wio[i, j] += _speed * errors[j] * _in[i];
+                }
+            }
+        }
+
+        public void SetRecurrentContext(Neuron context)
+        {
+            for(int i = 0; i < _out.Length; i++)
+            {
+                _out[i] += context._out[i];
+            }
+        }
+
+    }
+}
---
+++ RecurrentNN/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrentNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] pat = new double[][]
+            {
+                new double[] { 1 , 1 },
+                new double[] { 0 , 0 },
+                new double[] { 1 , 0 },
+                new double[] { 0 , 1 }
+            };
+
+            double[] a = new double[] { 0, 0, 1, 1 };
+
+            Neuron n = new Neuron(2, 2, new double[] { 0, 1 }, 0.01);
+            Neuron c = new Neuron(2, 2, new double[] { 0, 1 }, 0.01);
+            Neuron o = new Neuron(2, 1, new double[] { 0, 1 }, 0.01);
+
+            double ge = 0;
+            do
+            {
+                ge = 0;
+                for(int p = 0; p < pat.Length; p++)
+                {
+                    n.SetIn(pat[p]);
+                    n.Out();
+                    n.SetRecurrentContext(c);
+                    c.SetIn(n._out);
+                    c.Out();
+                    o.SetIn(n._out);
+                    o.Out();
+
+                    List<double> e = new List<double>();
+                    for(int i = 0; i < o._out.Length; i++)
+                    {
+                        e.Add(a[p] - o._out[i]);
+                        ge += Math.Abs(e[i]);
+                    }
+
+                    List<double[]> e2 = new List<double[]>();
+                    for(int i = 0; i < o._out.Length; i++)
+                    {
+                        List<double> cur = new List<double>();
+
+                        for (int j = 0; j < o._in.Length; j++)
+                        {
+                            cur.Add(e[i] * o._wio[j, i]);
+                        }
+                        e2.Add(cur.ToArray());
+                    }
+
+                    o.Study(e.ToArray());
+                    
+                    foreach (double[] d in e2)
+                    {
+                        c.Study(d);
+                        n.Study(d);
+                    }
+                }
+
+
+                Console.WriteLine(ge);
+            }
+            while (ge > 0);
+
+            Console.WriteLine();
+
+            for (int p = 0; p < pat.Length; p++)
+            {
+                n.SetIn(pat[p]);
+                n.Out();
+                n.SetRecurrentContext(c);
+                c.SetIn(n._out);
+                c.Out();
+                o.SetIn(n._out);
+                o.Out();
+                foreach (double d in o._out)
+                {
+                    Console.WriteLine(d);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
